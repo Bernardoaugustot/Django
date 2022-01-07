194ad9212bb0d2cd548b34e245ebebfe75fe4515
Bernardo Augusto <bernardo.augustot@gmail.com>
2022-01-07 12:15:26 -0300

    URL viewer e templates
    Aqui chegando no momento de configurar a exibição do site, as urls foram configuradas mas ao tentar lincar os templates o arquivo não esta conseguindo traquear corretamente.

--- projeto/polls/urls.py
+++ projeto/polls/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+# urlpatterns são as regras de caminhos que temos dentro do site, path recebe (caminho, onde vai abrir, nome exibido)
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    #Ppath('') esta dizendo que quando nenhum slug for informado ele vai cair nessa pag como defaul.
+    #as_view() é quem é responsavel pelas chamadas da URL quando estamos trabalhando no modelo de Class (oque estamos afaznedo aqui)
+    path('', views.PostListView.as_view(), name="list"),
+    path("<slug:slug>/", views.PostDetailView.as_view(), name="detail")
 ]
